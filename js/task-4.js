a34a6ea327a80bde2b9ec9ade0987aa16924fd25
Yana Ushakova <115149484+yana1982@users.noreply.github.com>
2024-04-05 22:42:40 +0300

    Update task-4.js

--- js/task-4.js
+++ js/task-4.js
@@ -1,8 +1,26 @@
 'use strict';
+let price;
 function getShippingCost(country) {
-    switch
-`Shipping to ${country} will cost ${price} credits`
-`Sorry, there is no delivery to your country`
+    switch (country) {
+        case 'China':
+        price = 100;
+        return `Shipping to ${country} will cost ${price} credits`;
+        break;
+        case 'Chile':
+        price = 250;
+        return `Shipping to ${country} will cost ${price} credits`;
+        break;
+        case 'Australia':
+        price = 170;
+        return `Shipping to ${country} will cost ${price} credits`;
+        break;
+        case 'Jamaica':
+        price = 120;
+        return `Shipping to ${country} will cost ${price} credits`;
+        break;
+        default:
+        return `Sorry, there is no delivery to your country`;
+    }
     }
 console.log(getShippingCost("Australia"));
 console.log(getShippingCost("Germany"));
